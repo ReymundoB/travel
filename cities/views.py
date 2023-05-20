57c473b7eaf3e52eb8da8d1db4b3e7dbcc39172e
ReymundoB <reymundo.b.z@gmail.com>
2023-05-20 16:57:15 -0600

    update-sign in and logout, add-register

--- cities/views.py
+++ cities/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from .models import City,Comment
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.models import User
+
 
 from .forms import Commentform
 
 
 
 def index(request):
+    print('Entramos al index')
     cities = City.objects.all()
 
     return render(request, 'cities.html', {'cities':cities})
@@ -37,3 +43,46 @@ def create_new_comment(request, id):
     else:
         return redirect('city', id=id)
         
+
+def loggin(request):
+    print('Entro en loggin')
+    if request.method == 'GET':
+        return render(request, 'registration/login.html',{
+            'form':AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        print('Valor de user:', user)
+        if user is None:
+            return render(request,'registration/login.html',{
+                'form':AuthenticationForm,
+                'error':'Username or password is incorrect'
+            })
+        else:
+            login(request,user)
+            return redirect('index')
+        
+
+def loggout(request):
+    logout(request)
+    return redirect('index')
+
+
+def register(request):
+    if request.method == 'GET':
+        return render(request,'registration/register.html',{
+            'form':UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user= User.objects.create_user(username=request.POST['username'],
+                                               password=request.POST['password1'])
+                user.save()
+                login(request,user)
+                return redirect('index')
+            except:
+                return render(request,'registration/register.html',{
+                    'form':UserCreationForm,
+                    'error':'Username already exists'
+                })
